4018037358b575e700132d88146066fac7be32a8
Moein Banihashemian <moein.banihashemian@gmail.com>
2014-11-09 20:06:26 +0100

    initial source

---
+++ magic8/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  magic8
+//
+//  Created by Moein Banihashemian on 27/10/14.
+//  Copyright (c) 2014 Moein Banihashemian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *ibiOutput;
+- (IBAction)btnAsk:(id)sender;
+
+@end
+
---
+++ magic8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  magic8
+//
+//  Created by Moein Banihashemian on 27/10/14.
+//  Copyright (c) 2014 Moein Banihashemian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

27f980f9099034c6cac396e89e6ba1ca28c8f5fc
Moein Banihashemian <moein@axxessios-MacBook-Pro.local>
2016-08-22 12:36:04 +0200

    magic string 3.5

--- magic8/ViewController.m
+++ magic8/ViewController.m
@@ -59,7 +59,7 @@ NSMutableArray* magicStrings;
     magicStrings[23]=@"feature3";
     magicStrings[24]=@"release-v1.0";
     magicStrings[25]=@"Bug2.5";
-    magicStrings[26]=@"Bug3";
+    magicStrings[26]=@"Bug3.5";
     magicStrings[27]=@"Bug4";
     magicStrings[28]=@"MainMenu-bug";
     magicStrings[29]=@"MainMenu";
